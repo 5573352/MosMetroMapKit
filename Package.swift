0f870289284f57974d41ce1389b51d51482ac89b
Павел Кузин <kuzin-pa@mosmetro.ru>
2021-08-30 10:34:21 +0300

    Russian is default

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MosMetroMapKit",
-    defaultLocalization: "en",
+    defaultLocalization: "ru",
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
