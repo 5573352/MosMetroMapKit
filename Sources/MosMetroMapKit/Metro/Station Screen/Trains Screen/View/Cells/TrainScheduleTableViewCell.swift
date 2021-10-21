e450baa9ac62ced4ac4e4d88ae63f51d43e3d701
Павел Кузин <kuzin-pa@mosmetro.ru>
2021-10-21 15:56:16 +0300

    Поправил локализацию

--- Sources/MosMetroMapKit/Metro/Station Screen/Trains Screen/View/Cells/TrainScheduleTableViewCell.swift
+++ Sources/MosMetroMapKit/Metro/Station Screen/Trains Screen/View/Cells/TrainScheduleTableViewCell.swift
@@ -14,6 +14,11 @@ class TrainScheduleTableViewCell: UITableViewCell {
     @IBOutlet weak private var evenStack      : UIStackView!
     @IBOutlet weak private var oddStack       : UIStackView!
     
+    @IBOutlet weak var firstTrainLeft: UILabel!
+    @IBOutlet weak var LastTrainLeft: UILabel!
+    @IBOutlet weak var FirstTrainTop: UILabel!
+    @IBOutlet weak var lastTrainTop: UILabel!
+    
     var viewState: StationTrainsScheduleController.ViewState.ScheduleData! {
         didSet {
             self.mainTitleLabel.text = viewState.title
@@ -26,5 +31,12 @@ class TrainScheduleTableViewCell: UITableViewCell {
                 last.text = viewState.oddTimes[safe: 1] ?? ""
             }
         }
+    }
+    
+    override func awakeFromNib() {
+        firstTrainLeft.text = NSLocalizedString("First train", tableName: nil, bundle: .mm_Map, value: "", comment: "")
+        LastTrainLeft.text = NSLocalizedString("Last train", tableName: nil, bundle: .mm_Map, value: "", comment: "")
+        FirstTrainTop.text = NSLocalizedString("Even", tableName: nil, bundle: .mm_Map, value: "", comment: "")
+        lastTrainTop.text = NSLocalizedString("Odd", tableName: nil, bundle: .mm_Map, value: "", comment: "")
     }
 }
